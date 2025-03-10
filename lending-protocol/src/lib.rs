85bb377acaedd15d1138ffa0b1f844fd19e88157
R. Vignolo <ramirovignolo@gmail.com>
2025-03-10 13:57:26 -0300

    feat: :sparkles: add Hatom's proxy files (#1)
    * feat: :sparkles: add Hatom's proxy files
    
    * ci: :green_heart: try fix ci

---
+++ lending-protocol/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+pub mod controller_proxy;
+pub mod discount_rate_model_proxy;
+pub mod interest_rate_model_proxy;
+pub mod money_market_proxy;
+pub mod oracle_proxy;
+pub mod ush_money_market_proxy;
---
+++ liquid-staking/src/lib.rs
@@ -0,0 +1,6 @@
+#![no_std]
+
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+pub mod liquid_staking_proxy;
